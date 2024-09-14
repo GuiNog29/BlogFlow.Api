621170d47721af9a8eb1f289b010a930214958fa
Guilherme Almeida <guimathiasgui@gmail.com>
2024-09-14 14:28:54 -0300

    Criando repositories

---
+++ BlogFlow.Domain/Interfaces/IUsuarioRepository.cs
@@ -0,0 +1,13 @@
+﻿using BlogFlow.Domain.Entities;
+
+namespace BlogFlow.Domain.Interfaces
+{
+    public interface IUsuarioRepository
+    {
+        Task<Usuario> Cadastrar(Usuario usuario);
+        Task<Usuario?> Visualizar(int usuarioId);
+        Task<bool> Editar(Usuario usuario);
+        Task<bool> Excluir(int usuarioId);
+        Task<IEnumerable<Usuario>> Listar(int usuarioId);
+    }
+}
---
+++ BlogFlow.Infrastructure/Repositories/PostagemRepository.cs
@@ -0,0 +1,73 @@
+﻿using BlogFlow.Domain.Entities;
+using BlogFlow.Domain.Interfaces;
+using BlogFlow.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogFlow.Infrastructure.Repositories
+{
+    public class PostagemRepository : IPostagemRepository
+    {
+        private readonly AppDbContext _dbContext;
+        public PostagemRepository(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Postagem> Cadastrar(Postagem postagem)
+        {
+            await _dbContext.Postagens.AddAsync(postagem);
+            await _dbContext.SaveChangesAsync();
+            return postagem;
+        }
+
+        public async Task<Postagem?> Visualizar(int postagemId)
+        {
+            return await ExistePostagem(postagemId);
+        }
+
+        public async Task<bool> Editar(Postagem postagem)
+        {
+            if (await ExistePostagem(postagem.Id) == null)
+                return false;
+
+            var local = _dbContext.Set<Usuario>()
+                          .Local
+                          .FirstOrDefault(entry => entry.Id.Equals(postagem.Id));
+
+            if (local != null)
+                _dbContext.Entry(local).State = EntityState.Detached;
+
+            _dbContext.Postagens.Attach(postagem);
+            _dbContext.Entry(postagem).State = EntityState.Modified;
+
+            await _dbContext.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<bool> Excluir(int postagemId)
+        {
+            var usuario = await ExistePostagem(postagemId);
+            if (usuario == null)
+                return false;
+
+            _dbContext.Postagens.Remove(usuario);
+            await _dbContext.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<IEnumerable<Postagem>> Listar()
+        {
+            return await _dbContext.Postagens.Include(p => p.Usuario).ToListAsync();
+        }
+
+        public async Task<IEnumerable<Postagem>> ListarPostagemUsuario(int usuarioId)
+        {
+            return await _dbContext.Postagens.Include(p => p.Usuario).Where(p => p.UsuarioId == usuarioId).ToListAsync();
+        }
+
+        private async Task<Postagem?> ExistePostagem(int postagemId)
+            => await _dbContext.Postagens
+                               .Include(p => p.Usuario)
+                               .FirstOrDefaultAsync(p => p.Id == postagemId);
+    }
+}
