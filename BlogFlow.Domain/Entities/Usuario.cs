2a6d31deff091655b7dd2f130fb1c0fed088498d
Guilherme Almeida <guimathiasgui@gmail.com>
2024-09-14 12:42:28 -0300

    Criando entidades e relacionamento das tabelas

---
+++ BlogFlow.Domain/Entities/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlogFlow.Domain.Entities
+{
+    public class Usuario
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public required string Nome { get; set; }
+        public required string Email { get; set; }
+        public required string Senha { get; set; }
+        public DateTime DataCriacao { get; set; }
+        public bool EhAdmin { get; set; } = false;
+        public ICollection<Postagem>? Postagens { get; set; }
+    }
+}
--- BlogFlow.Infrastructure/Data/AppDbContext.cs
+++ BlogFlow.Infrastructure/Data/AppDbContext.cs
@@ -1,9 +1,23 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BlogFlow.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace BlogFlow.Infrastructure.Data
 {
     public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Postagem> Postagens { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Postagem>()
+               .HasOne(p => p.Usuario)
+               .WithMany(p => p.Postagens)
+               .HasForeignKey(p => p.UsuarioId)
+               .OnDelete(DeleteBehavior.Restrict);
+        }
     }
 }
