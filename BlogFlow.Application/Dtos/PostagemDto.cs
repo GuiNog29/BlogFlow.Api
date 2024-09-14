8ba13b1780604db43e95a57ab9f5f766fe06f88a
Guilherme Almeida <guimathiasgui@gmail.com>
2024-09-14 14:28:42 -0300

    Criando DTOs

---
+++ BlogFlow.Application/Dtos/PostagemDto.cs
@@ -0,0 +1,11 @@
+﻿namespace BlogFlow.Application.Dtos
+{
+    public class PostagemDto
+    {
+        public int Id { get; set; }
+        public required string Titulo { get; set; }
+        public required string Conteudo { get; set; }
+        public int UsuarioId { get; set; }
+        public required UsuarioDto Usuario { get; set; }
+    }
+}
